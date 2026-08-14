4f1281b1f6fb273b2841c9ee376b4a5a38f6865c
Santiago Ruiz <sruiz557@unab.edu.co>
2026-08-13 22:36:54 -0500

    Creacion de rutas en web.php
    Se agregan las rutas /perfil, /perfil/intereses, /perfil/habilidades
    y /perfil/metas, manteniendo la ruta raiz que apunta a la vista welcome.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Rutas del sitio "Mi Perfil"
+|--------------------------------------------------------------------------
+|
+| Cada ruta responde con una vista Blade ubicada en resources/views.
+|
+*/
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+
+Route::get('/perfil/intereses', function () {
+    return view('intereses');
+});
+
+Route::get('/perfil/habilidades', function () {
+    return view('habilidades');
+});
+
+Route::get('/perfil/metas', function () {
+    return view('metas');
 });
